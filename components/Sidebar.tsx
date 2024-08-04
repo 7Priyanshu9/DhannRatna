74b2c6e7e32ed8150638758e58c28d9d7a6c57ec
7Priyanshu9 <priyanshupaul32@gmail.com>
2024-08-04 13:10:12 +0530

    added logos and updates the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,17 +1,45 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 const Sidebar = ({user}:SiderbarProps) => {
+
+  const pathName = usePathname();
+  // this will give the path//
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
-            <Link className='mb-12 cursor-pointer items-center gap-2' href="/">
+            <Link href="/" className='mb-12 cursor-pointer items-center gap-2' >
             <Image 
-            src="/icons/logo.svg"
+            src="./icons/logo1.svg"
             width={34}
             height={34}
+            alt='DhannRatna Logo'
+            className='size-[24px] max-xl:size-14'
             />
+            <h1 className='sidebar-logo'>DhannaRatna</h1>
             </Link>
+
+
+            {sidebarLinks.map((item)=>{
+// here we are checking either we are in current path or not 
+              const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`)
+              return (
+                <Link href={item.route} key={item.label} className={cn('sidebar-link',{'bg-bank-gradient': isActive})}>
+                  <div className="relative size-6">
+                    <Image 
+                    src={item.imgURL}
+                    alt={item.label}
+                    fill
+                    />
+                  </div>
+
+                </Link>
+              )
+            })}
         </nav>
     </section>
   )
